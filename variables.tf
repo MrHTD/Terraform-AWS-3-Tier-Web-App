339c19a375568a12b8f946695ea07c55d7b75bdf
MrHTD <mughis01@gmail.com>
2025-08-14 19:44:53 +0500

    refactor: clean up Terraform configuration and remove unused files

--- variables.tf
+++ variables.tf
@@ -1,25 +1,5 @@
-variable "vpc_id" {
-  type = string
-}
-
-variable "my_ip_with_cidr" {
+variable "aws_region" {
+  description = "Aws region to deploy resources"
   type        = string
-  description = "provide your ip"
-}
-
-variable "public_key" {
-  type        = string
-  description = "provide your ssh key"
-}
-
-variable "instance_type" {
-  type        = string
-  default     = "t2.micro"
-  description = "provide instance type"
-}
-
-variable "server_name" {
-  type        = string
-  default     = "Apache Example Server"
-  description = "provide server name"
+  default     = "us-east-1"
 }
